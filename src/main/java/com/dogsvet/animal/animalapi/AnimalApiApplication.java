0b1292978cd83ff3905326ac52502da43177f1ae
Bruno Souto <65253099+rhattox@users.noreply.github.com>
2023-12-21 21:04:28 -0300

    Add basic server

---
+++ src/main/java/com/dogsvet/animal/animalapi/AnimalApiApplication.java
@@ -0,0 +1,13 @@
+package com.dogsvet.animal.animalapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimalApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimalApiApplication.class, args);
+	}
+
+}
